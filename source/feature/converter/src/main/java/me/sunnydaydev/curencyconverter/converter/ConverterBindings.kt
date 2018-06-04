a2ea096dec7d11b15b3d2fbf0a1805236430b843
Aleksandr Tcikin <sunnyday.development@gmail.com>
2018-06-04 16:54:20 +0300

    Remove crop transformation (cropped on server).

--- source/feature/converter/src/main/java/me/sunnydaydev/curencyconverter/converter/ConverterBindings.kt
+++ source/feature/converter/src/main/java/me/sunnydaydev/curencyconverter/converter/ConverterBindings.kt
@@ -1,11 +1,6 @@
 package me.sunnydaydev.curencyconverter.converter
 
-import android.graphics.Bitmap
-import com.bumptech.glide.load.Transformation
-import com.bumptech.glide.load.engine.bitmap_recycle.BitmapPool
-import com.bumptech.glide.load.resource.bitmap.BitmapTransformation
 import me.sunnydaydev.mvvmkit.binding.RecyclerViewBindingsAdapter
-import java.security.MessageDigest
 
 /**
  * Created by sunny on 30.05.2018.
@@ -14,44 +9,7 @@ import java.security.MessageDigest
 
 internal class ConverterBindings {
 
-    companion object {
-
-        @JvmStatic
-        val flagTransformation: Transformation<Bitmap> get() = FlagTransformation()
-
-    }
-
     val currencyItemsMap = RecyclerViewBindingsAdapter.BindingMap(R.id.binding_converter_itemsMap)
             .map<CurrencyItemViewModel>(BR.vm, R.layout.converter_currency_item_layout)
-
-}
-
-/**
- * Original flags has bottom and top offsets, so we need to crop it.
- */
-private class FlagTransformation: BitmapTransformation() {
-
-    companion object {
-
-        private const val VERSION = 1
-
-        private const val ID = "me.sunnydaydev.ConverterFlagTransformation:$VERSION"
-
-    }
-
-    override fun updateDiskCacheKey(messageDigest: MessageDigest) {
-        messageDigest.update(ID.toByteArray())
-    }
-
-    override fun transform(pool: BitmapPool, toTransform: Bitmap, outWidth: Int, outHeight: Int): Bitmap {
-        val offset = (toTransform.height * 0.2).toInt()
-        return Bitmap.createBitmap(
-                toTransform,
-                0, offset,
-                toTransform.width, toTransform.height - offset * 2
-        )
-
-    }
-
 
 }
